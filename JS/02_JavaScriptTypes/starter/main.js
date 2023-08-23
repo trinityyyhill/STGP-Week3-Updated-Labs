b608bbe1d2726b3db1ff349bf86e629eee3774c5
Trinity Hill <hilltrinity@icloud.com>
2023-08-23 15:57:57 +0100

    model

---
+++ JS/02_JavaScriptTypes/starter/main.js
@@ -0,0 +1,15 @@
+let numTest = 45.324568;
+console.log(numTest);
+let twoDecimalPoints = numTest.toFixed(2);
+console.log(twoDecimalPoints);
+let stringTest = `I am the very model of a modern major general`;
+stringTest = stringTest.toUpperCase();
+let indexOfM = stringTest.indexOf(`M`);
+console.log(indexOfM);
+let start = stringTest.indexOf("MODEL");
+let end = stringTest.lastIndexOf('MAJOR');
+console.log(start)
+console.log(end)
+let subStr = stringTest.substring(start, end);
+console.log(subStr);
+document.write("<p>" + subStr + "</p>");
