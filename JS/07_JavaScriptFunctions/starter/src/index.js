13b905b36a3a373a45ab1a6b8067e1de1c2d994a
Trinity Hill <hilltrinity@icloud.com>
2023-08-25 13:03:09 +0100

    function

--- JS/07_JavaScriptFunctions/starter/src/index.js
+++ JS/07_JavaScriptFunctions/starter/src/index.js
@@ -6,8 +6,62 @@ const movies = [
     {"title": "Justice League", "director": "Zack Snyder", "year": "2017"}
 ];
 
-// Part 1 - Create a function to find a movie and output it's detail
-        
-        
+function findMovie(movieTitle) {
+    for (var movie of movies) {
+      if (movie.title === movieTitle) {
+        console.log(`Movie Title: ${movie.title}, Year: ${movie.year}`);
+      }
+      console.log("Value of movie:", movie);
+    }
+  }
+  
+findMovie("Star Wars");
+let movie = "Thor: Ragnorok";
+console.log(movie);
+findMovie(movie);
+function returnMovie(movieTitle) {
+    {
+        // Instruction 2-a
+        for (let movie of movies) {
+            // Instruction 2-a-i
+            if (movie.title === movieTitle) {
+                // Instruction 2-a-ii
+                return movie;
+            }
+            // Instruction 2-a-iii
+            console.log(movie);
+        }
+        // Instruction 2b
+        console.log("Any text, any text at all!");
+        // Instruction 8 - comment out line above and add line below
+        return `Movie not found`;
+    }    
+}
 
-// Part 2 = Create a function to return a movie object
+// Instruction 3
+let myMovie = returnMovie("Avengers: Infinity War");
+// Instruction 4
+console.log(myMovie);
+// Instruction 5
+console.log(`${myMovie.title} is a film directed by ${myMovie.director} released in ${myMovie.year}`);
+
+// Instruction 6
+let myOtherMovie = returnMovie("Thor: Ragnorok");
+// Instruction 7
+console.log(myOtherMovie);
+
+// Instruction 10
+function myMovieDetails(anyMovie) {
+    // Instruction 11
+    if (typeof (anyMovie) === 'object') {
+        return `${anyMovie.title} is a film directed by ${anyMovie.director} released in ${anyMovie.year}`;
+    } else {
+        return anyMovie;
+    }
+}
+
+// Instruction 12
+console.log(myMovieDetails(myOtherMovie));
+
+// Instruction 14 - a function as an argument to another function
+console.log(myMovieDetails(returnMovie("Jaws")));
